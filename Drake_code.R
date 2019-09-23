40e308981ff956d9643c33d8a698c1cf7acfeb0a
hanndrake <55696220+hanndrake@users.noreply.github.com>
2019-09-23 09:10:27 -0300

    Create Drake_code.R

---
+++ Drake_code.R
@@ -0,0 +1,6 @@
+lyrics = c("You used to call me on my, you used to, you used to
+You used to call me on my cell phone")
+
+print(lyrics)
+
+#prints the first lyrics of Drake's 2015 hit single "Hotline Bling"
